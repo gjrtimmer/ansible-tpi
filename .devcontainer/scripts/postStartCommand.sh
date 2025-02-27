2faa033487dfa8f4fe645623f86cacdad44b3eba
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-02-27 18:36:39 -0500

    feat: update postStartCommand

--- .devcontainer/scripts/postStartCommand.sh
+++ .devcontainer/scripts/postStartCommand.sh
@@ -4,3 +4,7 @@
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 DEVCON_DIR="$(dirname "${SCRIPT_DIR}")"
 CONFIG_DIR="${DEVCON_DIR}/config"
+
+echo "Configuring Git"
+# shellcheck disable=SC2154
+git config --global --add safe.directory /work
