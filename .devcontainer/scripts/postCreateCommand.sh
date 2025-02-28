a778eb1d901a9327a7e642ec28779464052ce2e0
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-02-28 10:26:59 +0000

    feat: fix rebuild container

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -6,7 +6,11 @@ DEVCON_DIR="$(dirname "${SCRIPT_DIR}")"
 CONFIG_DIR="${DEVCON_DIR}/config"
 
 if [[ ! -f "${DEVCON_DIR}/.bashrc" ]]; then
-    ln -sf "${DEVCON_DIR}/.bashrc" "${HOME}/.bashrc"
+    cp "${HOME}/.bashrc" "${DEVCON_DIR}/.bashrc"
+    
+    echo "export PROMPT_COMMAND='history -a'" >> "${DEVCON_DIR}/.bashrc"
+    echo "export HISTFILE=${DEVCON_DIR}/.bash_history" >> "${DEVCON_DIR}/.bashrc"
 fi
+ln -sf "${DEVCON_DIR}/.bashrc" "${HOME}/.bashrc"
 
 echo "Container Create Completed"
