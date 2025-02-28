a92c70a5d5a79321bab8e2dbc647b703d0f7624d
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-02-27 20:57:49 -0500

    feat: add inventory install

--- .devcontainer/scripts/postStartCommand.sh
+++ .devcontainer/scripts/postStartCommand.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 DEVCON_DIR="$(dirname "${SCRIPT_DIR}")"
 CONFIG_DIR="${DEVCON_DIR}/config"
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
 
 echo "Configuring Git"
 # shellcheck disable=SC2154
@@ -15,6 +16,11 @@ git config core.filemode false
 if [[ ! -f "${DEVCON_DIR}/.bashrc" ]]; then
     echo "Linking .bashrc"
     ln -sf "${DEVCON_DIR}/.bashrc" "${HOME}/.bashrc"
+fi
+
+if [ ! -f "${PROJECT_DIR}/hosts.yml" ]; then
+    echo "Installing Ansible Inventory Template"
+    cp "${PROJECT_DIR}/.config/hosts.tmpl.yml" "${PROJECT_DIR}/hosts.yml"
 fi
 
 # Install collections
