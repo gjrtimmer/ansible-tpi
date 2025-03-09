d324050c6abfd2dee7c8256a6814bff5fd77c23c
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-03-08 22:43:33 -0500

    feat: add ansible playground

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -5,11 +5,20 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 DEVCON_DIR="$(dirname "${SCRIPT_DIR}")"
 CONFIG_DIR="${DEVCON_DIR}/config"
 
+# Configure inputrc
+ln -sf "${CONFIG_DIR}/inputrc" "${HOME}/.inputrc"
+
+# .bashrc
 if [[ ! -f "${DEVCON_DIR}/.bashrc" ]]; then
     cp "${HOME}/.bashrc" "${DEVCON_DIR}/.bashrc"
     
     echo "export PROMPT_COMMAND='history -a'" >> "${DEVCON_DIR}/.bashrc"
     echo "export HISTFILE=${DEVCON_DIR}/.bash_history" >> "${DEVCON_DIR}/.bashrc"
+    
+    echo "alias ansible-playground='ansible -i /work/.config/hosts.playground.yml'" >> "${DEVCON_DIR}/.bashrc"
+    echo "alias ansible-playground-playbook='ansible-playbook -i /work/.config/hosts.playground.yml'" >> "${DEVCON_DIR}/.bashrc"
+    echo "alias ansible-pg='ansible -i /work/.config/hosts.playground.yml'" >> "${DEVCON_DIR}/.bashrc"
+    echo "alias ansible-pp='ansible-playbook -i /work/.config/hosts.playground.yml'" >> "${DEVCON_DIR}/.bashrc"
 fi
 ln -sf "${DEVCON_DIR}/.bashrc" "${HOME}/.bashrc"
 
