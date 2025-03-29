9f7d086ed724cbf35718b9c19ebe054d59f0c862
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-03-29 20:50:35 +0000

    chore: code format

--- .devcontainer/scripts/postStartCommand.sh
+++ .devcontainer/scripts/postStartCommand.sh
@@ -17,11 +17,11 @@ git -C "${PROJECT_DIR}" config core.eol lf
 sudo chmod -x ${HOME}/.ansible_vault_pass
 
 # Configure ownership mounts
-sudo chmod 0644 ${HOME}/.ssh/config
-sudo chmod 0644 ${HOME}/.ssh/known_hosts
-sudo chmod 0755 ${HOME}/.ssh/tpi
-sudo chmod 0600 ${HOME}/.ssh/tpi/*.key
-sudo chmod 0755 ${HOME}/.ssh/tpi/*.key.pub
+sudo chmod 0644 "${HOME}/.ssh/config"
+sudo chmod 0644 "${HOME}/.ssh/known_hosts"
+sudo chmod 0755 "${HOME}/.ssh/tpi"
+sudo chmod 0600 "${HOME}/.ssh/tpi/*.key"
+sudo chmod 0755 "${HOME}/.ssh/tpi/*.key.pub"
 sudo chown -R vscode:vscode ${HOME}/.ssh/tpi
 
 if [[ ! -f "${DEVCON_DIR}/.bashrc" ]]; then
